3f4dafd19518c1afbb1f850ccd518933834a982b
Dakota Benjamin <dakota.benjamin@gmail.com>
2017-09-30 00:00:33 -0400

    Add subtract feature

--- do_math.py
+++ do_math.py
@@ -1,6 +1,9 @@
 if __name__ == "__main__":
     x = 1
     y = 2
-    z = 1 + 2
+    z = x + y
+    subtract = z - x
     print("one plus 2 equals " + str(z))
+    print("z minus x equals" + str(subtract))
+
 
