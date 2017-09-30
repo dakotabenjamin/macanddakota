73a867aa274e59beb789304ff0f5a518b96084e8
Dakota Benjamin <dakota.benjamin@gmail.com>
2017-09-30 00:50:31 -0400

    Mutliplication

--- do_math.py
+++ do_math.py
@@ -4,7 +4,9 @@ if __name__ == "__main__":
     y = 2
     z = x + y
     subtract = z - x
+    multiply = y * z
     print("one plus 2 equals " + str(z))
     print("z minus x equals" + str(subtract))
+    print("y times z equals " + str(multiply))
 
 
